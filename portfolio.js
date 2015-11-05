849d8d0b3caa6506cd15445f660ce658ece1db5e
suhaibh <suhaib.habibullah@gmail.com>
2015-11-05 00:22:53 -0500

    Clean up fade in on scroll function

--- portfolio.js
+++ portfolio.js
@@ -11,13 +11,9 @@ $(document).ready(function(){
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 
 			if (bottom_of_window > bottom_of_object){
-				$(this).animate({opacity: 1}, 1500);	
-		}
-
+				$(this).animate({opacity: 1}, 1500);
+			};
 		})
-
-		
-		
 	})
 	
 });
