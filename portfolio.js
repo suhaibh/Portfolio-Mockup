1e955230ab7aaa260b11164315c6e482c4103f37
suhaibh <suhaib.habibullah@gmail.com>
2015-11-05 00:08:53 -0500

    Add skills images and fade in on scroll

--- portfolio.js
+++ portfolio.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
 			scrollTop: $("#portfolio").offset().top,
 		}, 1000);
 	})
+
+	$(window).scroll(function(){
+		$('.fadeInScroll').each(function(){
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			if (bottom_of_window > bottom_of_object){
+				$(this).animate({opacity: 1}, 1500);	
+		}
+
+		})
+
+		
+		
+	})
+	
 });
