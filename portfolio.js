bfcfedc0eb7c1b82826810ac6314a650ee2bec5e
suhaibh <suhaib.habibullah@gmail.com>
2015-11-11 00:07:52 -0500

    Remove fadeInScroll function and add function for intro box fade in on load with delay

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
+	
+	var introFadeIn = function(){
+		$('.intro-box').fadeIn(1000);
+	}
+
+	$(window).load(setTimeout(introFadeIn, 700))
+	
+
 	$('#portfolio-button').click(function(){
 		$('html,body').animate({
 			scrollTop: $("#portfolio").offset().top,
 		}, 1000);
-	})
-
-	$(window).scroll(function(){
-		$('.fadeInScroll').each(function(){
-			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-			var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-			if (bottom_of_window > bottom_of_object){
-				$(this).animate({opacity: 1}, 1500);
-			};
-		})
 	})
 	
 });
